45df09730c06343f4d0c63c111c9959241f352e2
marcocola2001 <cola01.mc@gmail.com>
2023-11-16 11:42:50 +0100

    VERDE: suonoLentoTest ora funziona

--- src/main/java/SlowTempoMusicalInstrument.java
+++ src/main/java/SlowTempoMusicalInstrument.java
@@ -8,10 +8,19 @@ public class SlowTempoMusicalInstrument implements MusicalInstrument {
 
     @Override
     public @NotNull String play() {
-        return null;
+        String suono = strumento.play();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < suono.length(); i++){
+            sb.append(suono.charAt(i));
+            if(isVocal(suono.charAt(i))){
+                sb.append(suono.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 
     private boolean isVocal(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
 }
